c3c3d1b2f4f18f157e7d72b8191e31f71b334c8f
fernandaday <fernandaiday@gmail.com>
2022-05-20 16:03:33 -0300

    Modulo 7 completado

---
+++ 7_REQ_HTTP_REACT/Fernanda/httpreact/src/App.js
@@ -0,0 +1,79 @@
+import './App.css';
+
+import { useState } from 'react'
+
+import { useFech } from './hooks/useFech'
+
+const url = 'http://localhost:3000/products'
+
+function App() {
+  // const [products, setProducts] = useState([])
+
+  const { data: items, httpConfig, loading, error } = useFech(url)
+  
+  const [name, setName] = useState("")
+  const [price, setPrice] = useState("")
+
+
+  const handleSubmit = async (e) =>{
+    e.preventDefault()
+
+    const product = {
+      name,
+      price
+    }
+
+    httpConfig(product, "POST")
+    
+    setName("")
+    setPrice("")
+  }
+
+  const handleDeleteProduct = (id) => {
+    httpConfig(id, "DELETE")
+  }
+
+  return (
+    <div className="App">
+      <h1>Lista de Produtos</h1>
+      {loading && <p>Carregando dados...</p>}
+      {error && <p>{error}</p>}
+      {!error &&(
+        <ul>
+          {items && items.map((product) => (
+            <li key={product.id}>
+              {product.name} - R$ {product.price}
+              <button 
+                onClick={() => {
+                  handleDeleteProduct(product.id)}}>Excluir</button>
+            </li>
+          ))}
+        </ul>
+      )}
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input 
+              type="text" 
+              value= {name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <label>
+            Preço:
+            <input 
+              type="number" 
+              value= {price}
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </label>
+          {loading && <input type="submit" disabled value="Aguarde..." />}
+          {!loading && <input type="submit" value="Criar" />}
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
